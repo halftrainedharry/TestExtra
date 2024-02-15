3424774700753dddd1f8fdac4094ca0ad1d29512
halftrainedharry <katachrese@gmx.ch>
2024-02-15 11:39:24 +0100

    Change GetList processors for Feature and Product

--- core/components/testextra/src/Processors/Feature/GetList.php
+++ core/components/testextra/src/Processors/Feature/GetList.php
@@ -3,6 +3,7 @@ namespace TestExtra\Processors\Feature;
 
 use MODX\Revolution\Processors\Model\GetListProcessor;
 use TestExtra\Model\Feature;
+use xPDO\Om\xPDOQuery;
 
 class GetList extends GetListProcessor
 {
@@ -12,4 +13,17 @@ class GetList extends GetListProcessor
 
     public $defaultSortField = 'id';
     public $defaultSortDirection = 'ASC';
+
+    public function prepareQueryBeforeCount(xPDOQuery $c)
+    {
+        $product_id = $this->getProperty('product_id');
+
+        if (!empty($product_id)) {
+            $c->where([
+                'product_id' => $product_id
+            ]);
+        }
+
+        return $c;
+    }
 }
--- core/components/testextra/src/Processors/Product/GetList.php
+++ core/components/testextra/src/Processors/Product/GetList.php
@@ -3,6 +3,8 @@ namespace TestExtra\Processors\Product;
 
 use MODX\Revolution\Processors\Model\GetListProcessor;
 use TestExtra\Model\Product;
+use TestExtra\Model\Feature;
+use xPDO\Om\xPDOQuery;
 
 class GetList extends GetListProcessor
 {
@@ -12,4 +14,17 @@ class GetList extends GetListProcessor
 
     public $defaultSortField = 'id';
     public $defaultSortDirection = 'ASC';
+
+    public function prepareQueryAfterCount(xPDOQuery $c)
+    {
+        $c->select($this->modx->getSelectColumns(Product::class, 'Product'));
+        // column with the count of available features of this product (that is then used in the grid)
+        $c->select(
+            [
+               '(SELECT COUNT(id) FROM ' . $this->modx->getTableName(Feature::class) . ' WHERE product_id = Product.id) AS features_count'
+            ]
+        );
+
+        return parent::prepareQueryAfterCount($c);
+    }
 }
