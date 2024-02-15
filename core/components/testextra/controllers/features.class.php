7d049aeb5d2594d8b92525ec06fc03c36a5a5e56
halftrainedharry <katachrese@gmx.ch>
2024-02-15 10:23:54 +0100

    Pass data from PHP to JS via variable

--- core/components/testextra/controllers/features.class.php
+++ core/components/testextra/controllers/features.class.php
@@ -1,11 +1,25 @@
 <?php
 require_once dirname(dirname(__FILE__)) . '/index.class.php';
 
+use TestExtra\Model\Product;
+
 class TestExtraFeaturesManagerController extends TestExtraBaseManagerController
 {
+    protected $product;
 
     public function process(array $scriptProperties = [])
     {
+        // Request Parameter für Karten-Gruppe auslesen
+        if (isset($scriptProperties['productid'])) {
+            $this->product = $this->modx->getObject(Product::class, [
+                'id' => $scriptProperties['productid']
+            ]);
+        }
+
+        if ($this->product === null) {
+            $this->failure('Product doesn\'t exist.');
+        }
+
         return '<div id="testextra-panel-features-div"></div>';
     }
 
@@ -16,6 +30,14 @@ class TestExtraFeaturesManagerController extends TestExtraBaseManagerController
 
     public function loadCustomCssJs(): void
     {
+        if ($this->product !== null) {
+            $this->addHtml('<script type="text/javascript">
+                Ext.onReady(function() {
+                    testextra.config.product_name = "' . addslashes($this->product->get('name')) . '";
+                });
+            </script>');
+        }
+
         $this->addLastJavascript($this->testextra->getOption('jsUrl') . 'mgr/widgets/features.panel.js');
         $this->addLastJavascript($this->testextra->getOption('jsUrl') . 'mgr/widgets/features.grid.js');
         $this->addLastJavascript($this->testextra->getOption('jsUrl') . 'mgr/sections/features.js');
